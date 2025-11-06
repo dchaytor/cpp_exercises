21199bc0a8b02a06979b79565034fc03d63e7160
dchayto <dlchaytor@outlook.com>
2025-11-05 19:33:47 -0500

    chapter 8, 12 exercises

---
+++ learncpp/ch12/12.x-5.cpp
@@ -0,0 +1,30 @@
+/* Write a function named sort2 which allows the caller to pass 2 int variables 
+   as arguments. When the function returns, the first argument should hold the 
+   lesser of the two values, and the second argument should hold the greater 
+   of the two values. */
+
+#include <iostream>
+
+constexpr void sort2(int& x, int& y)	{
+	if (y < x)	{
+		int temp { x };
+		x = y;
+		y = temp;
+	}
+}
+
+int main()
+{
+    int x { 7 };
+    int y { 5 };
+
+    std::cout << x << ' ' << y << '\n'; // should print 7 5
+
+    sort2(x, y); // make sure sort works when values need to be swapped
+    std::cout << x << ' ' << y << '\n'; // should print 5 7
+
+    sort2(x, y); // make sure sort works when values don't need to be swapped
+    std::cout << x << ' ' << y << '\n'; // should print 5 7
+
+    return 0;
+}
